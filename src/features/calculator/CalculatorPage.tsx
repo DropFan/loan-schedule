70f16bc569ff449704226e4dbbc18e0c7185feee
Tiger <DropFan@Gmail.com>
2026-03-29 10:21:29 +0800

    refactor: LoanSwitcher 移至 shared，改为下拉选择 + 重命名/删除

--- src/features/calculator/CalculatorPage.tsx
+++ src/features/calculator/CalculatorPage.tsx
@@ -1,6 +1,6 @@
 import { ChangeForm, ChangeTimeline } from '@/features/changes';
 import { LoanForm } from './components/LoanForm';
-import { LoanSwitcher } from './components/LoanSwitcher';
+import { LoanSwitcher } from '@/components/shared/LoanSwitcher';
 import { ScheduleTable } from './components/ScheduleTable';
 import { SummaryCards } from './components/SummaryCards';
 
